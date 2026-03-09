398629799039f772f2c72b0268c30b4613a5dd19
fishel <fishel@ut.ee>
2026-03-09 14:31:04 +0200

    splitting into shards added to tokenizer

--- tokandfilt.py
+++ tokandfilt.py
@@ -10,18 +10,40 @@ def tok(st):
     return re.sub(r'([.,!?;:"\'])', r" ▁\1", st)
 
 
-def parse(line, min_com):
+def parse(line, ofh, min_com):
     data = json.loads(line.strip())
 
-    if data['COMET'] >= min_com:
-        sr = tok(data['src_segm'])
-        tg = tok(data['tgt_segm'])
-        print(f"{sr} ||| {tg}")
+    if data['COMET'] < min_com:
+        return 0
+
+    sr = tok(data['src_segm'])
+    tg = tok(data['tgt_segm'])
+    print(f"{sr} ||| {tg}", file=ofh)
+    return 1
 
 
-def doit(fh, min_com):
+def output_filename(output_pref, file_idx):
+    return f"{output_pref}.{file_idx:03}.ali"
+
+def doit(fh, output_pref, min_com):
+    if output_pref is None:
+        ofh = sys.stdout
+        file_idx = None
+    else:
+        file_idx = 0
+        ofh = open(output_filename(output_pref, file_idx), 'w')
+    line_count = 0
+
     for l in fh:
-        parse(l, min_com)
+        line_count += parse(l, ofh, min_com)
+
+        if line_count >= 1000000 and file_idx is not None:
+            ofh.close()
+            file_idx += 1
+            ofh = open(output_filename(output_pref, file_idx), 'w')
+
+    if file_idx is not None:
+        ofh.close()
 
 
 if __name__ == "__main__":
@@ -30,4 +52,9 @@ if __name__ == "__main__":
     except:
         min_comet = MIN_COMET_DEFAULT
 
-    doit(sys.stdin, min_comet)
+    try:
+        output_file_preffix = sys.argv[2]
+    except:
+        output_file_preffix = None
+
+    doit(sys.stdin, output_file_preffix, min_comet)
